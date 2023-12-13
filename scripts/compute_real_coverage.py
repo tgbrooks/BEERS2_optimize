Fix computation of real coverage
Wasn't handling - strand and had reported transcript-relative position wrong

scripts/compute_real_coverage.py
@@ -29,13 +29,14 @@ transcript_ids = pl.DataFrame({"gene_id": gene_ids}).join(
 
 sequences = transcript_ids.join(gtf.filter(pl.col("feature") == "exon"), on="transcript_id", how="left") \
         .sort(["transcript_id", "start"]) \
-        .select("transcript_id", "seqname", "start", "end")
+        .select("transcript_id", "seqname", "start", "end", "strand")
 
 all_covs = []
 for (transcript_id, exons) in sequences.groupby("transcript_id"):
     pos = 1
-    for (_, chrom, start, end) in exons.iter_rows():
+    for (_, chrom, start, end, strand) in exons.iter_rows():
         # compute coverage of this exon
+        exon_len = end - start + 1
         region_str = f"{chrom}:{start}-{end}"
         cmd = ["samtools", "depth", bam_path, "-r", region_str, '-s', '-d', '0', '-a']
         p = subprocess.Popen(cmd, stdout=subprocess.PIPE)
@@ -44,14 +45,16 @@ for (transcript_id, exons) in sequences.groupby("transcript_id"):
             continue
         raw_cov = pandas.read_csv(io.StringIO(results), sep="\\s+", header=None)
         raw_cov.columns = ['chr', 'pos', 'depth']
+        direction = 1 if strand == "+" else -1
 
         # convert to transcript-based digits
         cov = pl.DataFrame({
             "transcript_id": transcript_id,
-            "pos": pos + np.arange(end-start+1),
-            "depth": raw_cov['depth'].values,
+            "pos": pos + np.arange(exon_len),
+            "depth": raw_cov['depth'].values[::direction],
         })
         all_covs.append(cov)
+        pos += exon_len
 all_covs = pl.concat(all_covs)
 
 all_covs.write_csv(snakemake.output.cov, separator="\t", has_header=False)

scripts/compute_scores.py
@@ -32,13 +32,13 @@ sim_cov = pl.read_csv(snakemake.input.sim_cov, separator="\t")
 real_cov = pl.read_csv(snakemake.input.real_cov, separator="\t")
 
 cov_dist = scipy.stats.wasserstein_distance(
-        sim_cov.filter(pl.col("transcript_length") < 1500)['coef_of_var'],
-        real_cov.filter(pl.col("transcript_length") < 1500)['coef_of_var'],
-    ) / real_cov.filter(pl.col("transcript_length") < 1500)['coef_of_var'].std()
+        sim_cov.filter(pl.col("transcript_length") < 3000)['coef_of_var'],
+        real_cov.filter(pl.col("transcript_length") < 3000)['coef_of_var'],
+    ) / real_cov.filter(pl.col("transcript_length") < 3000)['coef_of_var'].std()
 depth_dist = scipy.stats.wasserstein_distance(
-        sim_cov.filter(pl.col("transcript_length") > 1500)['exp_depth_pos_regression'],
-        real_cov.filter(pl.col("transcript_length") > 1500)['exp_depth_pos_regression'],
-    ) / real_cov.filter(pl.col("transcript_length") > 1500)['exp_depth_pos_regression'].std()
+        sim_cov.filter(pl.col("transcript_length") > 3000)['exp_depth_pos_regression'],
+        real_cov.filter(pl.col("transcript_length") > 3000)['exp_depth_pos_regression'],
+    ) / real_cov.filter(pl.col("transcript_length") > 3000)['exp_depth_pos_regression'].std()
 
 ### GC CONTENT
 # scored by earth-movers distance normalized by SD in real data